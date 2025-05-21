ecbfaecd0022f14bbcf46dafeed8d59d64e516e6
Paula Silva <paulanunes85@github.com>
2025-05-20 23:43:51 -0300

    Remove custom domain configuration and temporary files
    - Remove CNAME files for custom domain
    - Remove temp-unzip directory with temporary build files
    - Add .gitignore file with .claude directory and common patterns
    - Switch to standard GitHub Pages URL

--- temp-unzip/build/assets/js/a7456010.98a0c5bd.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkfigma_to_code_docusaurus=self.webpackChunkfigma_to_code_docusaurus||[]).push([[1235],{8552:u=>{u.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
